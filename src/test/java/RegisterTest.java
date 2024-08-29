84b5945f1bada6af4ba6d79c9137f0079905a009
Andrey <andreylenz42@yandex.ru>
2024-08-29 23:06:19 +0300

    Add firefox browser tests

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import page_object.LoginPage;
 import page_object.MainPage;
 import page_object.RegisterPage;
@@ -20,26 +21,36 @@ public class RegisterTest {
     String userEmail;
     String userPassword;
     boolean isSuccess;
-    public RegisterTest(String username,String userEmail,String userPassword,boolean isSuccess) {
+    String browser;
+    public RegisterTest(String username,String userEmail,String userPassword,boolean isSuccess,String browser) {
         this.username = username;
         this.userEmail = userEmail;
         this.userPassword = userPassword;
         this.isSuccess = isSuccess;
+        this.browser = browser;
     }
     @Parameterized.Parameters()
     public static Object[][] userDataInsert() {
         UserDataGenerator userDataGenerator = new UserDataGenerator();
 
         return new Object[][]{
-                {userDataGenerator.generateUserName(),userDataGenerator.generateUserEmail(),userDataGenerator.generateUserPassword(10,16),true},
-                {userDataGenerator.generateUserName(),userDataGenerator.generateUserEmail(),userDataGenerator.generateUserPassword(0,5),false}
+                {userDataGenerator.generateUserName(),userDataGenerator.generateUserEmail(),userDataGenerator.generateUserPassword(10,16),true,"chrome"},
+                {userDataGenerator.generateUserName(),userDataGenerator.generateUserEmail(),userDataGenerator.generateUserPassword(0,5),false,"chrome"},
+                {userDataGenerator.generateUserName(),userDataGenerator.generateUserEmail(),userDataGenerator.generateUserPassword(10,16),true,"firefox"},
+                {userDataGenerator.generateUserName(),userDataGenerator.generateUserEmail(),userDataGenerator.generateUserPassword(0,5),false,"firefox"}
 
     };
    }
     @Before
     public void setup() {
         // TODO: 29.08.2024 Сделать if на яндекс браузер
-        driver = new ChromeDriver();
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+        }
         driver.get("https://stellarburgers.nomoreparties.site/");
     }
     @Test
